2f11fa8c3cf72c1c323f2239c9c4df2ca7780565
ZerrY <suhan0304@gmail.com>
2024-07-05 16:23:28 +0900

    Update CircularDiffuseRippleAnimation.cs

--- ZerryLibrary/Assets/Scripts/CircularDiffuseRippleAnimation.cs
+++ ZerryLibrary/Assets/Scripts/CircularDiffuseRippleAnimation.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using System.Numerics;
 
 public class CircularDiffuseRippleAnimation : MonoBehaviour
 {
+    [SerializeField] CanvasGroup smallGlowCG;
+    [SerializeField] CanvasGroup bigGlowCG;
     void Start() {
-
+        DOTween.Sequence().SetLoops(-1, LoopType.Restart)
+            .Insert(0, smallGlowCG.transform.DOScale(1, 0.9f).From(0, true).SetEase(Ease.Linear))
+            .Insert(0, smallGlowCG.DOFade(0, 0.9f).From(0, true).SetEase(Ease.Linear))
+            .Insert(0.3f, bigGlowCG.transform.DOScale(1, 0.9f).From(0, true).SetEase(Ease.Linear))
+            .Insert(0.3f, bigGlowCG.DOFade(0, 0.9f).From(1,true).SetEase(Ease.Linear));
     }
 }
