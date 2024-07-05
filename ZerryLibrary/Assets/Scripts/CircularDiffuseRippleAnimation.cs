39221f2fee78617f31cfc661a1a729cef1d0ada5
ZerrY <suhan0304@gmail.com>
2024-07-05 16:26:08 +0900

    Update

--- ZerryLibrary/Assets/Scripts/CircularDiffuseRippleAnimation.cs
+++ ZerryLibrary/Assets/Scripts/CircularDiffuseRippleAnimation.cs
@@ -6,13 +6,13 @@ using System.Numerics;
 
 public class CircularDiffuseRippleAnimation : MonoBehaviour
 {
-    [SerializeField] CanvasGroup smallGlowCG;
-    [SerializeField] CanvasGroup bigGlowCG;
+    [SerializeField] CanvasGroup SmallGlowCG;
+    [SerializeField] CanvasGroup BigGlowCG;
     void Start() {
         DOTween.Sequence().SetLoops(-1, LoopType.Restart)
-            .Insert(0, smallGlowCG.transform.DOScale(1, 0.9f).From(0, true).SetEase(Ease.Linear))
-            .Insert(0, smallGlowCG.DOFade(0, 0.9f).From(0, true).SetEase(Ease.Linear))
-            .Insert(0.3f, bigGlowCG.transform.DOScale(1, 0.9f).From(0, true).SetEase(Ease.Linear))
-            .Insert(0.3f, bigGlowCG.DOFade(0, 0.9f).From(1,true).SetEase(Ease.Linear));
+            .Insert(0, SmallGlowCG.transform.DOScale(1, 0.9f).From(0, true).SetEase(Ease.Linear))
+            .Insert(0, SmallGlowCG.DOFade(0, 0.9f).From(1, true).SetEase(Ease.Linear))
+            .Insert(0.3f, BigGlowCG.transform.DOScale(1, 0.9f).From(0, true).SetEase(Ease.Linear))
+            .Insert(0.3f, BigGlowCG.DOFade(0, 0.9f).From(1, true).SetEase(Ease.Linear));
     }
 }
