f067a6a98c2c2304e6f31496e08b72ae141f4f01
ZerrY <suhan0304@gmail.com>
2024-07-05 17:06:30 +0900

    Update

---
+++ ZerryLibrary/Assets/Scripts/LightFlickerAnimation.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class LightFlickerAnimation : MonoBehaviour
+{
+    [SerializeField] private Image lightImg;
+    [SerializeField] private AudioSource flickerSound;
+
+    // Light off flicker
+    lightImg
+    
+}
