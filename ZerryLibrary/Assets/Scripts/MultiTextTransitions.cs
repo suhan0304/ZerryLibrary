71af4c32d4deb6cd2f6aec4963290f52697caf1a
ZerrY <suhan0304@gmail.com>
2024-07-06 21:43:31 +0900

    Update

--- ZerryLibrary/Assets/Scripts/MultiTextTransitions.cs
+++ ZerryLibrary/Assets/Scripts/MultiTextTransitions.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class MultiTextTransitions : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] CanvasGroup[] textCGs;
+    [SerializeField] int slideYAmount = 30;
+    [SerializeField] float sildeDuration = 1;
+    [SerializeField] float waitDelayToHide = 0;
+
+    Sequence textSequence;
+
+    void Awake() {
+        textSequence = DOTween.Sequence().SetAutoKill(false).SetLoops(-1, LoopType.Restart);
+        for (int i = 0; i < textCGs.Length; i++) {
+            Transform textT = textCGs[i].transform;
+            float hideDuration = sildeDuration * .5f;
+            float startTime = i * (sildeDuration + waitDelayToHide + hideDuration);
+
+            textSequence.Insert(startTime, textCGs[i].DOFade(1, sildeDuration).From(0, true).SetEase(Ease.OutQuart))
+                .Join(textT.DOLocalMoveY(0, sildeDuration).From(-slideYAmount, true, true).SetEase(Ease.OutQuart))
+                .AppendInterval(waitDelayToHide)
+                .Append(textCGs[i].DOFade(0, hideDuration).SetEase(Ease.OutQuart))
+                .Join(textT.DOLocalMoveY(slideYAmount, sildeDuration).SetRelative().SetEase(Ease.OutQuart));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnDestroy(){
+        textSequence.Kill();    
     }
 }
