2e93a968bc8c645c8cc01aa4bf1b41906e4af88b
ZerrY <suhan0304@gmail.com>
2024-07-06 16:00:14 +0900

    Update LoadingUIAnimation_ScaleFade.cs

--- ZerryLibrary/Assets/Scripts/LoadingUIAnimation_ScaleFade.cs
+++ ZerryLibrary/Assets/Scripts/LoadingUIAnimation_ScaleFade.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class LoadingUIAnimation_ScaleFade : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+[SerializeField] float duration = 1f;
+    [SerializeField] float radius = 100;
+    [SerializeField] GameObject loadingElement;
+
+    private int count;
+    private float delayUnit;
+
+    private void Start() {
+        Play();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [Button("Plus Loading Element")]
+    private void plusLoadingElement() {
+        Instantiate(loadingElement, transform);
+        Init();
+    }
+
+    [Button("Init")]
+    [ContextMenu("Init")]
+    private void Init() {
+        count = transform.childCount == 0 ? 1 : transform.childCount;
+        delayUnit = duration / count;
+
+        int i = 0;
+        foreach (Transform child in transform) {
+            child.localPosition = new Vector2(Mathf.Sin(Mathf.PI * 2 * i / count) * radius, Mathf.Cos(Mathf.PI * 2 * i / count) * radius);
+            child.localRotation = Quaternion.LookRotation(Vector3.forward, new Vector3(child.localPosition.x, child.localPosition.y, 0));
+            i++;
+        }
+    }
+
+    [Button("Play")]
+    private void Play() {
+        Init();
+            float delay = 0;
+            foreach (Transform child in transform)
+            {
+                child.GetComponent<Image>().DOFade(0, 0);
+                child.GetComponent<Image>().DOFade(0, duration).From(1, false)
+                .SetDelay(delay)
+                .SetLoops(-1, LoopType.Restart)
+                .SetEase(Ease.Linear)
+                .SetLink(gameObject, LinkBehaviour.PauseOnDisablePlayOnEnable);
+
+                delay += delayUnit;
+            }
     }
 }
