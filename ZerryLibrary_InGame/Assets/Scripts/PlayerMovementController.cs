f766095c0975ec5bf2e410e674a294aba9e5341d
ZerrY <suhan0304@gmail.com>
2024-07-14 17:44:01 +0900

    Update

--- ZerryLibrary_InGame/Assets/Scripts/PlayerMovementController.cs
+++ ZerryLibrary_InGame/Assets/Scripts/PlayerMovementController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.AI;
 using static UnityEngine.InputSystem.InputAction;
 
 public class PlayerMovementController : MonoBehaviour
@@ -7,9 +8,20 @@ public class PlayerMovementController : MonoBehaviour
 
     private void Awake() => _inputMapping = new InputControls();
 
+    private Camera _camera;
+    private NavMeshAgent _agent;
+    private float _rotateSpeed = 5f;
+    private bool _needToRotate = false;
+
+    private Vector3 _moveTarget = Vector3.zero;
+    private Vector3 _direction = Vector3.zero;
+    private Quaternion _lookRotation = Quaternion.identity;
+
     void Start() {
         _inputMapping.Default.Walk.performed += Walk;
         _inputMapping.Default.Run.performed += Run;
+
+
     }
 
     private void OnEnable() => _inputMapping.Enable();
