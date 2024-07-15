8c258c35b549673ddb64d3a94871575683c13822
ZerrY <suhan0304@gmail.com>
2024-07-15 18:11:13 +0900

    Update CompassObjective.cs

--- ZerryLibrary_InGame/CompassAndObjectives/Assets/Scripts/CompassObjective.cs
+++ ZerryLibrary_InGame/CompassAndObjectives/Assets/Scripts/CompassObjective.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 
 public class CompassObjective : MonoBehaviour
@@ -16,6 +15,20 @@ public class CompassObjective : MonoBehaviour
     public CompassObjective Configure(GameObject worldGameObject, Color color, Sprite sprite = null) {
         WorldGameObject = worldGameObject.transform;
         _rectTransform = GetComponent<RectTransform>();
+
+        ObjectiveImage.color = color;
+
+        if (sprite != null) {
+            ObjectiveImage.sprite = sprite;
+        }
+
+        ObjectiveImage.transform.localScale = Vector3.zero;
+
+        UpdateCompassPosition();
+
+        return this;
     }
+
+    private void LateUpdate() => UpdateCompassPosition();
 
 }
