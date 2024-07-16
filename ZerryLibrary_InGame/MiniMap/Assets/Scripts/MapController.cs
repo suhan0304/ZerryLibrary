923a82489f57700a4d93a8a789325f52c88a264c
ZerrY <suhan0304@gmail.com>
2024-07-16 16:40:09 +0900

    Update MapController.cs

--- ZerryLibrary_InGame/MiniMap/Assets/Scripts/MapController.cs
+++ ZerryLibrary_InGame/MiniMap/Assets/Scripts/MapController.cs
@@ -8,8 +8,25 @@ public class MapController : MonoBehaviour
     private VisualElement _root;
     private bool IsMapOpen => _root.ClassListContains("root-container-full");
 
+    public GameObject Player;
+    [Range(1, 15)]
+    public float miniMultiplyer = 5.3f;
+    [Range(1, 15)]
+    public float fullMultiplyer = 7f;
+    private VisualElement _playerRepresentation;
+
     void Start() {
         _root = GetComponent<UIDocument>().rootVisualElement.Q<VisualElement>("Container");
+        _playerRepresentation = _root.Q<VisualElement>("Player");
+    }
+
+    private void LateUpdate()
+    {
+        var multiplyer = IsMapOpen ? fullMultiplyer : miniMultiplyer;
+        _playerRepresentation.style.translate = 
+            new Translate(Player.transform.position.x * multiplyer, Player.transform.position.z * -multiplyer, 0);
+        _playerRepresentation.style.rotate = 
+            new Rotate(new Angle(Player.transform.rotation.eulerAngles.y));
     }
 
     void Update() {
